2351fdd36b4a2914acfe6a97ebe016c5d0705f56
Harsh Rakholiya <106314499+RakholiyaHarsh8@users.noreply.github.com>
2025-03-07 19:30:16 +0000

    ok

--- 01_basic/01_variables.js
+++ 01_basic/01_variables.js
@@ -1,14 +1,60 @@
-const accountNumber = 50100300950953 //Change No Kari Sakay const Variable ne
-let accountEmail = "harsh@gmail.com" //change  Thay Sa ke let Variable
-var accountPassword = "4826" 
-accountCity = "Surat"
-let accountState; // Jo variable ma koy velue nathi aapata to undefinde value tha se
+// const accountNumber = 50100300950953 //Change No Kari Sakay const Variable ne
+// let accountEmail = "harsh@gmail.com" //change  Thay Sa ke let Variable
+// var accountPassword = "4826" 
+// accountCity = "Surat"
+// let accountState; // Jo variable ma koy velue nathi aapata to undefinde value tha se
 
 
-//accountNumber = 100300500850960 // Error Show Kar se = "Assignment to constant variable." --> Not Allows
-accountEmail = "harsh01@gmail.com"
-accountPassword = "7878"
-accountCity = "Ahmedabad"
+// //accountNumber = 100300500850960 // Error Show Kar se = "Assignment to constant variable." --> Not Allows
+// accountEmail = "harsh01@gmail.com"
+// accountPassword = "7878"
+// accountCity = "Ahmedabad"
 
-console.log(accountNumber,accountEmail,accountPassword,accountCity);
-console.table([accountNumber, accountEmail, accountPassword,accountCity,accountState])  //Table  Format ma lava mate...
+// console.log(accountNumber,accountEmail,accountPassword,accountCity);
+// console.table([accountNumber, accountEmail, accountPassword,accountCity,accountState])  //Table  Format ma lava mate...
+
+
+
+// var harsh = 10;
+// var harsh = 20
+
+
+// let harsh;
+//  harsh = 10;
+// console.log(harsh);
+
+
+// const harsh = 10;
+// console.log(harsh);
+
+
+// const cars = ["Saab", "Volvo", "BMW"];
+// cars[2] = "Audi"
+// console.log(cars);
+
+// let harsh = {};
+const harsh = [];
+
+harsh[0] = "rakholiya"
+harsh[1] = 22;
+harsh[2] = "BCA"
+
+console.log(harsh);
+
+// varible  = 
+// var = same name apaso to chalse, pasad thi value aapso to bhi chalse
+// let = same name nai chale , pasad thi value chalse
+// const = same name nai chale , pasaf thi value nai chale
+
+// Array
+// var = same name chalse , var x = [] chalse, pasad thi value chalse
+// let = same name nai chale, let x = [] chalse, pasad thi value chalse 
+// conast = same name nai chale, const x = [] chalse, pasad thi value chalse 
+
+// Object
+// var = same name chalse , var x = {} chalse, pasad thi value chalse
+// let = same name nai chale, let x = {} chalse, pasad thi value chalse 
+// conast = same name nai chale, const x = {} chalse, pasad thi value chalse 
+
+// video jovana 6 
+// ojbect, array, function, String, number, if else , switch, loop 
