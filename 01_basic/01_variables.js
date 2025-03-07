3241ed3b96f36c30c02b25b2820449341692a6ad
Harsh Rakholiya <106314499+RakholiyaHarsh8@users.noreply.github.com>
2025-03-07 10:28:30 +0000

    Variables

---
+++ 01_basic/01_variables.js
@@ -0,0 +1,14 @@
+const accountNumber = 50100300950953 //Change No Kari Sakay const Variable ne
+let accountEmail = "harsh@gmail.com" //change  Thay Sa ke let Variable
+var accountPassword = "4826" 
+accountCity = "Surat"
+let accountState; // Jo variable ma koy velue nathi aapata to undefinde value tha se
+
+
+//accountNumber = 100300500850960 // Error Show Kar se = "Assignment to constant variable." --> Not Allows
+accountEmail = "harsh01@gmail.com"
+accountPassword = "7878"
+accountCity = "Ahmedabad"
+
+console.log(accountNumber,accountEmail,accountPassword,accountCity);
+console.table([accountNumber, accountEmail, accountPassword,accountCity,accountState])  //Table  Format ma lava mate...
