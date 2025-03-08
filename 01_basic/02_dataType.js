abc9350ac63fcb3dcebbb15e6ab189de727ae500
Harsh Rakholiya <106314499+RakholiyaHarsh8@users.noreply.github.com>
2025-03-08 12:18:32 +0000

    DataType

---
+++ 01_basic/02_dataType.js
@@ -0,0 +1,36 @@
+"use strict"; //treat all JS code as newer version
+
+//alert(3 + 3) //we are using nodejs, not brower
+
+
+// console.log(3 
+//     + 3)    // code readability should be high
+// console.log("HARSH")    
+
+
+// dataType
+
+let fname = "HARSH" //String DataType Output = HARSH
+let Age = 21 //Number DataType OutPut = 21
+let number = null //Null Ek Object se with datatype OutPut = Object -> Empty VAlue
+let isLoggedIn = 0 //Boolen DataType se output = false and ture
+let state //undefind //Datatype se OutPut = undefined se
+
+//console.log(typeof (Age,fname))
+
+// number => 2 to 53
+// BigInt => Log Value Leva mate 
+//           jem ke stock market number, bankblase, EventCounts.
+// String => "HARSH" => ""
+//Boolean = false/ture
+//null => object null one type
+// Symbol => unique
+
+const cars = ["Saab",5,"HARSH"];
+
+console.log(typeof cars[0]);
+console.log(typeof cars[1]);
+console.log(typeof cars[2]);
+
+
+
