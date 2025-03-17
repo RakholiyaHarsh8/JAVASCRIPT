aeac3a98b8bb3b6fbd04a9398fbd5c6f3b92ad4d
Harsh Rakholiya <106314499+RakholiyaHarsh8@users.noreply.github.com>
2025-03-17 03:40:59 +0000

    Oprations

---
+++ 01_basic/03_oprations.js
@@ -0,0 +1,44 @@
+let value = 3
+let nageValue = -value
+console.log(nageValue)
+
+
+// Two String Join in oprations
+let str1 = "DEMO"
+let str2 = " HARSH"
+
+let str3 = str1 + str2
+console.log(str3);
+
+
+console.log("1" + 2); //Not
+console.log("1" + 2 + 2); //Not
+console.log(1 + "2"); //Not
+console.log(1 + 2 + "2"); //Not Allow
+
+
+console.log(3 + 4 * 5 % 6 / 10); //Not WOrk in Company --> Only Exam Mate
+console.log( (3 + 4) * 5 % 6 / 10); //Allow Code in Company
+
+
+console.log(true); //Allow code
+console.log(+true); //Not Allow Conversion
+
+console.log(+""); //Not Allow
+
+//==================================================================================================================
+
+//Not Allow Code in Compnay
+let num1, num2,num3
+
+num1 = num2 = num3 = 2 + 2 
+
+//==============================================================================
+
+let gameCounter1 = 100
+gameCounter1++; // incremate
+console.log(gameCounter1);
+
+let gameCounter2 = 100
+++gameCounter2; // incremate
+console.log(gameCounter2);
