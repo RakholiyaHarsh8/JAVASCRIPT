d4489e284bd5bfe6cb9681c7a062b805a8c6a1f4
Harsh Rakholiya <106314499+RakholiyaHarsh8@users.noreply.github.com>
2025-03-17 03:41:17 +0000

    Comparision

---
+++ 01_basic/04_comparision.js
@@ -0,0 +1,15 @@
+
+// console.log(null < 0); //null convert kar diya 0 me
+// console.log(null > 0); //null convert kar diya 0 me
+// console.log(null == 0); //null convert kar diya 0 me
+// console.log(null >= 0); //null convert kar diya 0 me
+
+// console.log(undefined == 0); //undefind convert kar diya 0 me
+
+//=====================================================================
+
+// == 
+console.log("2" == 3) // Allow For Code Conversion Thay Se Atale --> OutPut = True
+
+//  ===
+console.log("2" === 3); //DataType Check Thay Se Atale Not Allow --> OutPut = False
